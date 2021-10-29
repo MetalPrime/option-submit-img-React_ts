4471a3c7d1c2b4252b97e460d140a3b48b2e1f13
Jhon Garcia <metalprime57@gmail.com>
2021-10-28 19:02:17 -0500

    update in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
 
-function App() {
+ function App() { 
+  //ete ref es deldiv de la imagen del perfil
+   const ref  = useRef<HTMLImageElement>(null);
+  
+  //cuando se hace el submit pasa esto
+  // esto deberia pasar dentro del visualizador
+  const handleSubmit = (event: any) => {
+    console.log(event.target.files[0]);
+    const reader = new FileReader();
+    console.log(reader);
+    reader.onload = (((e: ProgressEvent<FileReader>)=>{
+      //referencia al objeto img 
+      
+      if(ref.current?.src){
+        if(typeof e.target?.result! === 'string'){
+          //aqui es donde sucede la asignación del src
+          ref.current.src = e.target?.result!;
+          console.log(e.target?.result);
+        } else {
+          console.log("Porfavor adjunte una imagen válida")
+        }
+
+      }else {
+        console.log("La referencia es nula");
+      }
+     
+      
+    }) as any)
+    reader.readAsDataURL(event.target.files[0]);
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <input multiple accept=".jpg,.png,.webp,.jfif" type="file" name="img" id="img-profile" onChange={handleSubmit} />
+      <button>Submit</button>
+      
+      <img src="" alt="" ref={ref}/>
+    
     </div>
-  );
-}
+  ); 
+
+
+  // initiazalite el dato : let, (var) or const -- tipo de dato -- valor inicial
+
+  
+
+ } 
+
 
 export default App;
